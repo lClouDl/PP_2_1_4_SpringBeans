0dd50c5ff9b7f2e502162e5265258de21fd5db73
Konstantin Graf <cloud_33@mail.ru>
2023-01-23 21:43:12 +0700

    1) Создал бин Dog, унаследовал его от Animal. Создал на основе этого класса компонент. 2) Связал AnimalCage c бином Dog через абстрактный класс Animal. 4) На основе класса Timer создал бин. Связал с AnimalCage. Проверил, что при выполнении метода main время, которое пишет таймер, одно и тоже. 5) Раскоментировал тест из заготовки и проверил своё решение. 6) Создал дополнительный метод getTimer() в классе AnimalCage, так как этого требует тест.

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,12 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component("Dog")
+public class Dog extends Animal {
+
+    @Override
+    public String toString() {
+        return "I'm Dog";
+    }
+}
